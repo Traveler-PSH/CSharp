f29122ee7b12b5b55d9614cac320cc8fced4d14b
Tim Park <littletreel0@naver.com>
2024-09-01 23:44:21 +0900

    48

--- Prac_book32~/Program.cs
+++ Prac_book32~/Program.cs
@@ -4,8 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("two sentence");
+            float f = 5.2f;
+            double d = 10.5;
+            decimal money = 200.099m;
+
+            Console.WriteLine(f);
+            Console.WriteLine(d);
+            Console.WriteLine(money);
+
+            char ch = '\u2023';
+            Console.WriteLine(ch);
+
+            string text = "\"Hello World\"";
+            Console.WriteLine(text);
+
+            String text2 = @"\tHello\nWorld";
+            Console.WriteLine(text2);
         }
     }
 }
